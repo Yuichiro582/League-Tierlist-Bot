38c44b2cc0b2d8796d95cad6862bc3669421514b
Yuichiro582 <yoyo41112@gmail.com>
2026-05-28 06:10:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-index.js
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+    console.log(`Bot online as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
+
